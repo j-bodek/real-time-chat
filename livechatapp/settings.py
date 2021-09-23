82b6ca98a4d77b754490f165a2ecd5c0fea5bd34
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-23 19:14:57 +0200

    setting_channels

--- livechatapp/settings.py
+++ livechatapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'livechatapp.wsgi.application'
+
+#setting asgi application
+ASGI_APPLICATION = "livechatapp.asgi.application"
 
 
 # Database
