15be50c58f7132dc6f121349c3b129cfcc5ffcef
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-25 23:50:17 +0200

    livechat

--- livechatapp/settings.py
+++ livechatapp/settings.py
@@ -75,7 +75,14 @@ WSGI_APPLICATION = 'livechatapp.wsgi.application'
 
 #setting asgi application
 ASGI_APPLICATION = "livechatapp.asgi.application"
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
