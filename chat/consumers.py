b9f92403a1261e99cabe5817427f35e84574fa70
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-26 19:48:18 +0200

    sender_receiver

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,11 +2,18 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+import random
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return random.randint(range_start, range_end)
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+        self.scope["session"]["seed"] = random_with_N_digits(8)
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -27,19 +34,28 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        user = self.scope['session']['seed']
+        message = str(user) + message
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event['message']
+
+        sender = event['message'][0:8]
+        message = event['message'][8:]
+
+        if self.scope['session']['seed'] == int(sender):
+            message = 'You: ' + message
+        else:
+            message = 'Stranger: ' + message
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
