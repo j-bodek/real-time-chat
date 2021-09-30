86fd41da319d9c58d27b6dd3c44ad40c7fc8a798
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-30 18:35:55 +0200

    'heroku_settings'

--- livechatapp/settings.py
+++ livechatapp/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "livechatapp.settings")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
