4092f48e7e7c29bd11626146460267942fd11b74
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-30 18:11:09 +0200

    heroku_settings

--- livechatapp/asgi.py
+++ livechatapp/asgi.py
@@ -8,19 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livechatapp.settings')
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-      "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-}) 
+django.setup()
+
+application = get_default_application()
