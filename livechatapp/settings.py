47976908615f51a7760fcab56125051932e9f98c
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-30 16:09:24 +0200

    heroku_database

--- livechatapp/settings.py
+++ livechatapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4=alh9pij9xe1v))0!_xwo7lrql4nan6yrsnej7z!=g#cf=fo_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['masny-chat.herokuapp.com']
 
 
 # Application definition
@@ -93,10 +94,10 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'chat',
-        'USER': 'postgres',
-        'PASSWORD':'J@kub2002',
-        'HOST':'localhost',
+        'NAME': 'd24rokfhrqu2es',
+        'USER': 'yhwochwzvgxxxy',
+        'PASSWORD':'037afae75bde0e79ee4cbe42bcd726c98725a27c3cc436f1ec5def19dcb887c3',
+        'HOST':'ec2-63-33-239-176.eu-west-1.compute.amazonaws.com',
         'POST':'5432',
     }
 }
@@ -138,11 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
